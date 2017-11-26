71cde318993c05decc02821b1a541c2bc7c6919d
Mustafa GUNES <gunes149@gmail.com>
2017-11-26 14:40:56 +0300

    Project was created

---
+++ IntelligentImage(MachineLearning)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntelligentImage(MachineLearning)
+//
+//  Created by Mustafa GUNES on 26.11.2017.
+//  Copyright © 2017 Mustafa GUNES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func changeClicked(_ sender: Any) {
+        
+        
+    }
+}
+
